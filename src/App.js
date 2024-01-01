e1a3cfdfc5ed858b9b31f673e1e4f076fb07d441
Priyanshu <Priyanshujoshi2020@gmail.com>
2024-01-01 20:32:01 +0530

    completed the weather regulator

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
+import React, { useState } from "react";
 import "./App.css"
 
 
 
+const App = () => {
 
-
-
-
-const User = ({name, age , email}) => {
-  return(
-  <div>
-    <h1>{name}</h1>
-    <h1> {age}</h1>
-    <h1>{email}</h1>
-
-  </div>
-  )
-}
-
-const Salary = ({position, Salary}) => {
-
-  return(
-    <div className="Salary"  >
-
-      <h1>Position: {position}</h1>
-      <h1> {Salary}</h1>
-
-
-    </div>
-
-  )
-
-}
-
-
-
-function App() { 
-  const age = 19;
-  const isGreen = true;
   
 
-  return (
-    
-    <div className="App">
-     
-     {age >= 18 ? <h1>over age</h1> : <h1> under age </h1>}
-     <h1 style={{color: isGreen ? "green" : "red"}}>This has color </h1>
+  const [temp, setTemp] = useState(14)
+  const [color, setColor] = useState("normal")
 
-      {/* <Salary position="SDE1" Salary={12000}></Salary>
-      <Salary position="SDE2" Salary={15000}></Salary>
-      <User name =" rahul"></User>
-     <User name="rahul" age={21} email="rahul@rahul.com" ></User>
-     <User name="rahul" age={21} email="rahul@rahul.com" ></User>
-     <User name="rahul" age={21} email="rahul@rahul.com" ></User> */}
-    </div>
-  );
+  let increase = () =>{
+   const newTemp = temp+1
+
+  
+
+    if(newTemp >= 15){
+      setColor("hot")
+    }
+
+      setTemp(newTemp)
+
+      if(newTemp == 24){
+        alert("temperature is max")
+        setTemp(14)
+       }
+  }
+
+  let decrease = () =>{
+
+   const newTemp = temp-1
+
+    if(newTemp <= 12){
+      setColor("cold")
+    }
+
+      setTemp(newTemp)
 }
 
-export default App;
+
+
+
+  return (
+    <div>
+      <div className="container">
+
+        <div className=" temp-ConatinerSpan">
+        <span className = {`tempSpan ${color}`}  > {temp}&deg;C </span>
+         </div>
+        
+        <div className="button">
+        <button onClick={increase}> + </button>
+         <button onClick={decrease}> - </button>
+        </div>
+
+
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default App
